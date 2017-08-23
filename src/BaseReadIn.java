911ba675e6ef89879ee75d5b586e47b731fe3ae4
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2017-08-23 13:17:56 -0500

    Added Basics

--- src/BaseReadIn.java
+++ src/BaseReadIn.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class BaseReadIn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+
+        for (int i = 0; i<test; i++){
+
+        }
+
+    }
 }
